d202c7d32b0ab2bf155c952db8529617ca178b0c
Zoid <zaid19competente@gmail.com>
2025-10-31 17:35:11 +0800

    New UI

--- app/src/main/java/com/example/librarymanagementsystem_users/MainActivity.java
+++ app/src/main/java/com/example/librarymanagementsystem_users/MainActivity.java
@@ -13,30 +13,10 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button signup_button;
-    private Button login_button;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.welcome_screen);
-
-        login_button = findViewById(R.id.login_button);
-        signup_button = findViewById(R.id.signup_button);
-
-        login_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-               setContentView(R.layout.sign_in);
-            }
-        });
-
-        signup_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                setContentView(R.layout.sign_up);
-            }
-        });
     }
 }
