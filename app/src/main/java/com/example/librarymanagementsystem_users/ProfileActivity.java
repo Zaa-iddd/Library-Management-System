63e4986abe7569e84f089ed561b40f2ce4f4c500
laurence gomez <glaurence09611@gmail.com>
2025-11-20 13:05:52 -0700

    Added profile back func

--- app/src/main/java/com/example/librarymanagementsystem_users/ProfileActivity.java
+++ app/src/main/java/com/example/librarymanagementsystem_users/ProfileActivity.java
@@ -1,14 +1,26 @@
 package com.example.librarymanagementsystem_users;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    ImageView backButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile_info);
+
+        backButton = findViewById(R.id.backButton);
+
+        backButton.setOnClickListener(v -> {
+            Intent intent = new Intent(ProfileActivity.this, MainDashActivity.class);
+            startActivity(intent);
+            finish(); // Optional: finish ProfileActivity so it's not in the back stack
+        });
     }
 }
