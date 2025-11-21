179172c0b8355977e2396db042462a55307d38c1
Zoid <zaid19competente@gmail.com>
2025-11-21 09:09:32 +0800

    Bsta dagdag

---
+++ app/src/main/java/com/example/librarymanagementsystem_users/functions/Book.java
@@ -0,0 +1,8 @@
+package com.example.librarymanagementsystem_users.functions;
+
+public class Book extends LibraryItem{
+    @Override
+    public void displayInfo() {
+
+    }
+}
---
+++ app/src/main/java/com/example/librarymanagementsystem_users/functions/LibraryItem.java
@@ -0,0 +1,8 @@
+package com.example.librarymanagementsystem_users.functions;
+
+public abstract class LibraryItem {
+    public void borrow() {
+        System.out.println("Item has been borrowed.");
+    }
+    public abstract void displayInfo();
+}
